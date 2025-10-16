82ef5f2136a3e958c46442e849e3e7e5bbcc3e5e
Da-ma3022 <damariz.gonzalez3022@alumnos.udg.mx>
2025-10-16 09:59:36 -0600

    Agregue los modulos de splash(logo), menu(con las tarjetas), modulo de sueño mas trabajado, bienestar, temporizador, y los botones

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.testapp.ajustes.AjustesScreen
 import com.example.testapp.bienestar.BienestarScreen
 import com.example.testapp.calendario.CalendarioScreen
 import com.example.testapp.menu.MenuScreen
 import com.example.testapp.plan_de_estudios.PlanDeEstudiosScreen
 import com.example.testapp.splash.SplashScreen
+import com.example.testapp.sueño.PlanDetalleScreen
 import com.example.testapp.sueño.SueñoScreen
 import com.example.testapp.temporizador.TemporizadorScreen
 import com.example.testapp.ui.theme.TestAppTheme
@@ -34,6 +37,17 @@ class MainActivity : ComponentActivity() {
                         composable("plan_de_estudios") { PlanDeEstudiosScreen(navController) }
                         composable("calendario") { CalendarioScreen(navController) }
                         composable("ajustes") { AjustesScreen(navController) }
+
+                        // Nueva ruta para el detalle del plan
+                        composable(
+                            route = "plan_detalle/{planId}",
+                            arguments = listOf(navArgument("planId") { type = NavType.StringType })
+                        ) { backStackEntry ->
+                            PlanDetalleScreen(
+                                navController = navController,
+                                planId = backStackEntry.arguments?.getString("planId")
+                            )
+                        }
                     }
                 }
             }
